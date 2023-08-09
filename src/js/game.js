106d2ad1fb347d1f4bdc1c77244fe45a0e34db30
Yuriy Mironenko <ydtalel@gmail.com>
2023-08-09 15:32:22 +0300

    first commit

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+
+import domainCharacter from  './domain';
+
+export class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
